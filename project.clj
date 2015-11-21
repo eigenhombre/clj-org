5fe250dbb8718ddca28853524d7445fb9146cac7
John Jacobsen <john@mail.npxdesigns.com>
2015-11-20 20:25:47 -0600

    Tweak version for Clojars.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-org "0.1.0-SNAPSHOT"
+(defproject clj-org "0.0.1-SNAPSHOT"
   :description "A Parser for Emacs Org Mode Files"
   :url "https://github.com/eigenhombre/clj-org"
   :license {:name "MIT"}
