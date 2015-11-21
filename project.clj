7824493108e3c417a37d0c71b61734130668cea0
John Jacobsen <john@mail.npxdesigns.com>
2015-11-20 20:34:41 -0600

    Switch to Speclj for tests.

--- project.clj
+++ project.clj
@@ -2,4 +2,9 @@
   :description "A Parser for Emacs Org Mode Files"
   :url "https://github.com/eigenhombre/clj-org"
   :license {:name "MIT"}
+  :aliases {"autotest" ["spec" "-a"]
+            "autoquiet" ["spec" "--format=progress" "-r" "v"]}
+  :test-paths ["spec"]
+  :profiles {:dev {:dependencies [[speclj "3.2.0"]]
+                   :plugins [[speclj "3.2.0"]]}}
   :dependencies [[org.clojure/clojure "1.7.0"]])
