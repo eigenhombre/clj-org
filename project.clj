485e42b10815a0980c4ee8d633cc4e9527910f56
John Jacobsen <john@mail.npxdesigns.com>
2015-11-20 20:24:18 -0600

    In the beginning was chaos.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-org "0.1.0-SNAPSHOT"
+  :description "A Parser for Emacs Org Mode Files"
+  :url "https://github.com/eigenhombre/clj-org"
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
---
+++ src/clj_org/core.clj
@@ -0,0 +1,1 @@
+(ns clj-org.core)
