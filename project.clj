dbfd51829f55ea92932e161e5fe039957337591b
John Jacobsen <john@mail.npxdesigns.com>
2015-11-20 21:29:06 -0600

    Let's release this suckah.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-org "0.0.1-SNAPSHOT"
+(defproject clj-org "0.0.1"
   :description "A Parser for Emacs Org Mode Files"
   :url "https://github.com/eigenhombre/clj-org"
   :license {:name "MIT"}
