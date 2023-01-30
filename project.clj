9532c846de46c8d76b8e0859ce06a418f0790990
John Jacobsen <eigenhombre@gmail.com>
2023-01-29 21:06:46 -0600

    Version 0.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-org "0.0.3"
+(defproject clj-org "0.0.4-SNAPSHOT"
   :description "A Parser for Emacs Org Mode Files"
   :url "https://github.com/eigenhombre/clj-org"
   :license {:name "MIT"}
