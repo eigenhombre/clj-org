3c36e07983478fd369862f5f8800006d9907256d
John Jacobsen <eigenhombre@gmail.com>
2023-01-29 21:04:05 -0600

    Add release boilerplate. #6

--- project.clj
+++ project.clj
@@ -7,4 +7,21 @@
   :profiles {:dev {:dependencies []
                    :plugins [[jonase/eastwood "0.9.9"]
                              [lein-bikeshed "0.5.2"]
-                             [lein-kibit "0.1.8"]]}})
+                             [lein-file-replace "0.1.0"]
+                             [lein-kibit "0.1.8"]]}}
+  :deploy-repositories [["releases" :clojars]]
+  :repositories [["releases" {:url "https://repo.clojars.org"
+                              :creds :gpg}]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version"
+                   "release"]
+                  ["file-replace" "README.md"
+                   "\\[eigenhombre/clj-org \"" "\"]"
+                   "version"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "v" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
